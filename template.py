de3eaf3d2e907324862deec97118ee72caf687b4
Rahul <krahulgovind@gmail.com>
2024-06-30 20:47:27 +0530

    Deleting all the files and starting again due to incorrect project_name

--- template.py
+++ template.py
@@ -2,12 +2,14 @@ import os
 from pathlib import Path
 import logging
 
+# It means whenever we use logging.info, it will print the message in the told format only i.e. timestamp and then the  message, and logging has many other levels like debug, warning, error, etc. and each levels are used for different purposes, you can explore them in the official documentation clearly.
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "cnnClassifier"
+project_name = "myFashionRecommender"
 
 list_of_files = [
+    # The reason for creating .gitkeep file is to make sure that the directory is created in the git repository, because git does not allow to push empty directories, so we need to create a file in the directory to make sure that the directory is created in the git repository.
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
@@ -29,7 +31,10 @@ list_of_files = [
 
 
 for filepath in list_of_files:
+    # Path(filepath) is used to convert the string into a path object, so that we can use the path object to get the directory name and the file name, and also to check if the file exists or not.
+    # Plus we know ".github/workflows/.gitkeep" in windows we use backslash and in linux we use forward slash, so to make it platform independent we use Path(filepath) as it will automatically detect the operating system and convert it into the correct format.
     filepath = Path(filepath)
+    # ".github/workflows/.gitkeep" for this example filedir will be ".github/workflows" and filename will be ".gitkeep"
     filedir, filename = os.path.split(filepath)
 
 
