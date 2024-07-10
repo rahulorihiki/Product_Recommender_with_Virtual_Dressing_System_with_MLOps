d8ded65e739c9858ac2a7e340025a3357e3092c6
Rahul <krahulgovind@gmail.com>
2024-07-10 17:27:44 +0530

    Deployment Trial 13

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from main import app
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080) # Run the app on all IP addresses of the host machine on port 8080.
